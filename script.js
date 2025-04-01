46074cc70b69790010cc0b79abea7f5af72b198d
soda9091 <eestevass@gmail.com>
2025-04-01 13:49:22 -0600

    Update script.js
    agregamos flechas y supimos como usarlas

--- script.js
+++ script.js
@@ -51,3 +51,57 @@ function saludarUsuario(nombre){
 
 saludarUsuario("Juan"); // Llama a la funcion saludarUsuario con el nombre Juan
 saludarUsuario("Maria"); // Llama a la funcion saludarUsuario con el nombre Maria   
+
+//////////////////////////
+
+// funcion que calcula la suma de 5 numeros diferentes
+function suma(x, y, z){
+    return x + y + z;
+}
+
+suma(2, 5, 8);
+console.log(varlorDevuelto);
+
+// Ejemplo 2: almacenar el valor devuelto en una variable
+let valorDevuelto = suma(2, 5, 8); // 15
+console.log(valorDevuelto); // Imprime 15
+
+// Ejemplo 3: utilizarlo como parametro de una funcion
+function imprimirValor(valorDevuelto){
+    console.log(valorDevuelto);
+}
+imprimirValor(suma(2, 5, 8)); // Imprime 15
+
+// Ejemplo 4: utilizarlo en una estructura de control de flujo
+if (valorDevuelto >= 5) {
+    console.log("Es mayor"); //true")
+} else {
+    console.log("Es menor"); //false
+}
+
+
+///////////////
+
+// Funciones flecha
+
+/*
+
+Caracteristicas de esta funcion flecha
+- Se puede evitar colocar () si solo se usa un parametro.
+- Se puede evitar el uso de la clausula 'return' si solo existe una operacion.
+- Se puede evitar el uso de {} si solo se escribe una instruccion dentro de la funcion.
+
+*/
+
+// Funcion flecha que calcula el producto de dos numeros
+const productoFlecha = (x, y) => {
+    console.log("Resultados: "); // Multiplicacion de dos numeros
+    console.log(x * y);
+};
+
+productoFlecha(4, 6); // 24
+
+// Funcion flecha que saluda a un usuario
+const saludoFlecha = usuario => console.log("Hola, " + usuario); // Imprime Hola + nombre
+
+saludoFlecha("Magali"); // Llama a la funcion saludoFlecha con el nombre Magali
