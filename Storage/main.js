c7bad6b0d6d571a2b88c4e3be5f747886d4a0cfc
soda9091 <eestevass@gmail.com>
2025-04-09 13:39:48 -0600

    se van a agregar dos html uno de index y otro de welcome

--- Storage/main.js
+++ Storage/main.js
@@ -4,18 +4,22 @@ let email = localStorage.getItem("email");
 encabezado1.innerText =  'Bienvenido de nuevo ${email}';
 console.log(email);
 
+localStorage.removeItem("email"); //elimina el elemento email del localStorage
+
+//guarda el correo electronico en el navegador
+//localStorage.setItem("email", "eestevass@gmail.com")
+
+/////////////////////////
+
 let encabezado = document.getElementsByTagName("h1").item(0);
 
 if (localStorage.getItem("email") === null) {
     let email = localStorage.getItem("email");
     encabezado.innerText = 'Bienvenido de nuevo ${email}';
+
     console.log(email);
 } else {
     //guarda el correo electronico en el navegador
     localStorage.setItem("email","eestevass@gmail.com");
     localStorage.setItem("email","edu18cool@gmail.com");
 }//!==null
-
-
-//guarda el correo electronico en el navegador
-//localStorage.setItem("email", "eestevass@gmail.com")
